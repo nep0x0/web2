c9fb991fc9cc771dd3b3aed4427b58d966c83533
Niansyah Eko Putra <61461755+nep0x0@users.noreply.github.com>
2022-10-10 08:56:58 +0700

    add dokter/create route

--- 8020200101/8020200101-medik-app/routes/web.php
+++ 8020200101/8020200101-medik-app/routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Http\Controllers\DokterController;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('profil', [ProfilController::class,'index'])->name('user');
-
 Route::get('profil/create', [ProfilController::class,'create'])->name('user');
-Route::get('pelanggan', [PelangganController::class,'index'])->name('user');
+Route::get('dokter/create', [DokterController::class,'create'])->name('create');
+
+//Route::get('pelanggan', [PelangganController::class,'index'])->name('user');
 
 Route::get('/', function () {
     return view('welcome');
