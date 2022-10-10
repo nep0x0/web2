53b784e00c833b1ccc4dfd719de1eb6bcf1da7fb
Niansyah Eko Putra <61461755+nep0x0@users.noreply.github.com>
2022-10-10 09:08:46 +0700

    add new function
    1. dokter index
    2. dokter edit
    3. dokter 2

--- 8020200101/8020200101-medik-app/app/Http/Controllers/DokterController.php
+++ 8020200101/8020200101-medik-app/app/Http/Controllers/DokterController.php
@@ -6,8 +6,20 @@ use Illuminate\Http\Request;
 
 class DokterController extends Controller
 {
+    public function index()
+    {
+        return 'Halo, Saya berada di halaman dokter index';
+    }
     public function create()
     {
         return 'Halo, Saya dokter di fungsi create';
+    }
+    public function edit()
+    {
+        return 'Halo, saya berada dihalaman edit dengan nilai 1';
+    }
+    public function show()
+    {
+        return 'Halo, saya berada dihalaman show dengan nilai 2';
     }
 }
--- 8020200101/8020200101-medik-app/routes/web.php
+++ 8020200101/8020200101-medik-app/routes/web.php
@@ -6,11 +6,17 @@ use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('profil', [ProfilController::class,'index'])->name('user');
+/* Route::get('profil', [ProfilController::class,'index'])->name('user');
 Route::get('profil/create', [ProfilController::class,'create'])->name('user');
-Route::get('dokter/create', [DokterController::class,'create'])->name('create');
+Route::get('pelanggan', [PelangganController::class,'index'])->name('user');
+ */
 
-//Route::get('pelanggan', [PelangganController::class,'index'])->name('user');
+
+Route::get('dokter', [DokterController::class,'index'])->name('dokterindex');
+Route::get('dokter/create', [DokterController::class,'create'])->name('doktercreate');
+Route::get('dokter/1/edit', [DokterController::class,'edit'])->name('dokter1edit');
+Route::get('dokter/2', [DokterController::class,'show'])->name('dokter2show');
+
 
 Route::get('/', function () {
     return view('welcome');
